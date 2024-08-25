aeaf14f54ae0334af0b26b6a1e9049e86f801724
Camila Maximo <camilamximo20@gmail.com>
2024-08-25 11:32:31 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import ImcCalc from './assets/components/ImcCalc'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+  
+    <div className='container'>
+    <ImcCalc/>
+
+        
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/components/ImcCalc.jsx
@@ -0,0 +1,32 @@
+import Button from "./Button";
+
+import "./ImcCalc.css";
+
+const ImcCalc = () => {
+  return (
+    <div id="calc-container">
+      <h2>Calculadora do IMC</h2>
+      <form id="imc-form">
+        <div className="form-inputs">
+            <div className="form-control">
+                <label htmlFor="height">Altura:</label>
+                <input type="text" name="height" placeholder="Exemplo 1,75"/>
+            </div>
+            <div className="form-control">
+                <label htmlFor="weight">Peso:</label>
+                <input type="text" name="weight" placeholder="Exemplo 70,5"/>
+            </div>
+        </div>
+        <div className="action-control">
+
+           <Button id="calc-btn" text="Calcular"/>
+           <Button id="clear-btn" text="Limpar"/>
+
+        </div>
+      </form>
+
+    </div>
+  )
+}
+
+export default ImcCalc
